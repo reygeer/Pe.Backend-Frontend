ca2e2e0ef18f5c9d129cfdf7ea895569163d0a7e
reygeer <delarajuan2703@gmail.com>
2025-07-18 13:30:54 -0600

    Primer API conectada a Clever Cloud

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const proveedoresRoutes = require('./routes/proveedores');
+const clientesRoutes = require('./routes/clientes');
+
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+app.use('/api/proveedores', proveedoresRoutes);
+app.use('/api/clientes', clientesRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
