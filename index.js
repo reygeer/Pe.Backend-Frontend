4da44d6105c48e43c81adc5e2eb7a82604c5be8e
reygeer <delarajuan2703@gmail.com>
2025-07-22 05:45:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const app = express();
 
 const proveedoresRoutes = require('./routes/proveedores');
 const clientesRoutes = require('./routes/clientes');
-const importacionesRoutes = require('./routes/importaciones');
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
@@ -13,7 +12,6 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use('/api/proveedores', proveedoresRoutes);
 app.use('/api/clientes', clientesRoutes);
-app.use('/api/importaciones',importacionesRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
